339fc1214e747c62d442f0cd77c32e471f30fb66
José Raddaoui Marín <raddaouimarin@gmail.com>
2024-04-30 19:02:06 +0200

    Update config test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -11,19 +11,26 @@ import (
 
 const testConfig = `# Config
 debug = true
+verbosity = 2
+sharedPath = "/home/preprocessing/shared"
 [temporal]
 address = "host:port"
+namespace = "default"
+taskQueue = "preprocessing"
+workflowName = "preprocessing"
+[worker]
+maxConcurrentSessions = 1
 `
 
 func TestConfig(t *testing.T) {
 	tmpDir := fs.NewDir(
 		t, "",
 		fs.WithFile(
-			"preprocessing_sfa.toml",
+			"preprocessing.toml",
 			testConfig,
 		),
 	)
-	configFile := tmpDir.Join("preprocessing_sfa.toml")
+	configFile := tmpDir.Join("preprocessing.toml")
 
 	var c config.Configuration
 	found, configFileUsed, err := config.Read(&c, configFile)
@@ -31,5 +38,15 @@ func TestConfig(t *testing.T) {
 	assert.NilError(t, err)
 	assert.Equal(t, found, true)
 	assert.Equal(t, configFileUsed, configFile)
+
+	assert.Equal(t, c.Debug, true)
+	assert.Equal(t, c.Verbosity, 2)
+	assert.Equal(t, c.SharedPath, "/home/preprocessing/shared")
+
 	assert.Equal(t, c.Temporal.Address, "host:port")
+	assert.Equal(t, c.Temporal.Namespace, "default")
+	assert.Equal(t, c.Temporal.TaskQueue, "preprocessing")
+	assert.Equal(t, c.Temporal.WorkflowName, "preprocessing")
+
+	assert.Equal(t, c.Worker.MaxConcurrentSessions, 1)
 }
