f303168a2b5ff672fbd816675eed45ab5fab5026
David Juhasz <djjuhasz@gmail.com>
2025-03-14 16:58:02 -0700

    Improve content and system error messages
    Refs #43
    
    Update content and system error messages using the notes to provide
    additional context and recommended actions.
    
    [skip-codecov]

--- internal/eventlog/eventlog.go
+++ internal/eventlog/eventlog.go
@@ -1,7 +1,6 @@
 package eventlog
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/artefactual-sdps/preprocessing-sfa/internal/enums"
@@ -27,10 +26,10 @@ func (e *Event) IsSuccess() bool {
 	return e.Outcome == enums.EventOutcomeSuccess
 }
 
-func (e *Event) Complete(t time.Time, outcome enums.EventOutcome, msg string, a ...any) *Event {
+func (e *Event) Complete(t time.Time, outcome enums.EventOutcome, msg string) *Event {
 	e.CompletedAt = t
 	e.Outcome = outcome
-	e.Message = fmt.Sprintf(msg, a...)
+	e.Message = msg
 
 	return e
 }
