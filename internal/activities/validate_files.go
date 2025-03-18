0cc7b1550f9385466ee67022d90c43801b58bd07
Mike Cantelon <mcantelon@gmail.com>
2025-03-18 14:12:22 -0700

    move identification of files to fformat

--- internal/activities/validate_files.go
+++ internal/activities/validate_files.go
@@ -2,13 +2,8 @@ package activities
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"io/fs"
-	"path/filepath"
 	"slices"
-
-	"go.artefactual.dev/tools/temporal"
 
 	"github.com/artefactual-sdps/preprocessing-sfa/internal/fformat"
 	"github.com/artefactual-sdps/preprocessing-sfa/internal/fvalidate"
@@ -42,7 +37,7 @@ func NewValidateFiles(idr fformat.Identifier, vdrs ...fvalidate.Validator) *Vali
 // Execute validates SIP files against a file format specification. The
 // only format validator currently implemented verapdf for PDF/A.
 func (a *ValidateFiles) Execute(ctx context.Context, params *ValidateFilesParams) (*ValidateFilesResult, error) {
-	formats, err := a.identifyFormats(ctx, params.SIP)
+	formats, err := fformat.IdentifyFormats(ctx, a.identifier, params.SIP)
 	if err != nil {
 		return nil, fmt.Errorf("identifyFormats: %v", err)
 	}
@@ -55,6 +50,7 @@ func (a *ValidateFiles) Execute(ctx context.Context, params *ValidateFilesParams
 	return &ValidateFilesResult{Failures: failures}, nil
 }
 
+/*
 func (a *ValidateFiles) identifyFormats(ctx context.Context, sip sip.SIP) (fileFormats, error) {
 	logger := temporal.GetLogger(ctx)
 	formats := make(fileFormats)
@@ -86,10 +82,11 @@ func (a *ValidateFiles) identifyFormats(ctx context.Context, sip sip.SIP) (fileF
 
 	return formats, nil
 }
+*/
 
 func (a *ValidateFiles) validateFiles(
 	sip sip.SIP,
-	files fileFormats,
+	files fformat.FileFormats,
 ) ([]string, error) {
 	var failures []string
 	for _, v := range a.validators {
@@ -105,7 +102,7 @@ func (a *ValidateFiles) validateFiles(
 	return failures, nil
 }
 
-func validate(v fvalidate.Validator, path string, files fileFormats) (string, error) {
+func validate(v fvalidate.Validator, path string, files fformat.FileFormats) (string, error) {
 	var canValidate bool
 	allowedIds := v.FormatIDs()
 
